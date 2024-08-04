362805d22b5f535a80c59a9aa6f38016dd2fe21e
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2024-08-04 11:25:40 +0500

    Created script to generate barcode from json response

--- barcode_script.py
+++ barcode_script.py
@@ -1,14 +1,22 @@
 import time
 import tkinter as tk
+import json
+# always do this >> from file_name import module name 
+from json_extractor import json_extractor 
 from PIL import Image, ImageTk
 import barcode
 from barcode.writer import ImageWriter
 from io import BytesIO
 
-def generate_barcodes(start, count):
+# control_numbers = [13036279, 12973357, 13044726, 12906422, 12970010, 12970012, 12931597, 13077059, 13078048, 13028923, 13028953, 13031734, 13049053, 12907270, 13054229, 13079990, 13076948, 12962311, 13043825, 13060564, 13030395, 13048393, 13046479, 13063071, 13069494, 13070817, 13070827, 13070932, 13072497, 13073392, 13066827, 13073927, 13068505, 13070472, 13028280, 12965116, 12964672, 12922988, 13042647, 13025485, 13014798, 12902087, 13020307, 13073828, 13057655, 13057766, 12952279, 13058180, 13054177, 12903927, 12956852, 13024233, 13024622, 12965517, 13012637, 13035964, 13054111, 13066327, 13071011, 13076777, 13043152, 13020331, 13023329, 13026230, 13039155, 13039580, 12946363, 12951910, 12908138, 12917434, 12902958, 13057244, 13057273, 13078846, 13044800, 13049596, 13044305]
+
+json_file_path = "daily_route_sheet_response_D91798O.json" 
+control_numbers = json_extractor(json_file_path)
+
+def generate_barcodes(control_numbers):
     barcodes = []
-    for i in range(start, start + count):
-        barcode_value = f"C1|{i:06d}"
+    for control_number in control_numbers:
+        barcode_value = f"C1|{control_number}"
         barcodes.append(barcode_value)
     return barcodes
 
@@ -31,13 +39,13 @@ def display_barcode_images(barcodes):
         label.config(image=photo)
         label.image = photo
         root.update()
-        time.sleep(3)
+        time.sleep(2)
 
     root.destroy()
 
 if __name__ == "__main__":
-    start_number = 1
-    total_barcodes = 100  # Change this value to generate a different number of barcodes
+    # start_number = 1
+    # total_barcodes = 150  # Change this value to generate a different number of barcodes
     
-    barcodes = generate_barcodes(start_number, total_barcodes)
+    barcodes = generate_barcodes(control_numbers)
     display_barcode_images(barcodes)
---
+++ json_extractor.py
@@ -0,0 +1,37 @@
+import json
+
+def json_extractor(json_file=""):
+    # Parse the JSON data
+    try:
+        # Parse the JSON data
+        with open(json_file, 'r') as file:
+            file_content = file.read()
+            data = json.loads(file_content)
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+        return []
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+        return []
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+    
+    # Extract controlNumbers into a list
+    control_numbers = []
+    try:
+        for stop in data['routeSheetDTO']['stopsDTO']:
+            delivery_details = stop['details'].get('deliveryDetails')
+            if delivery_details:
+                packages = delivery_details.get('packageDTO', [])
+                for package in packages:
+                    control_numbers.append(package['controlNumber'])
+    except KeyError as e:
+        print(f"Key error: {e}")
+        return []
+    return (control_numbers)
+
+# json_file = "daily_route_sheet_response_S01162A.json"
+# control_numbers = json_extractor()
+# print(control_numbers)
+# json_extractor()
